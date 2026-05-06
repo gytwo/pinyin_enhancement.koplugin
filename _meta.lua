e71fdfe4f07cbfb4863ba3dd3b06c8c13b31f2c0
gytwo <15279172411@163.com>
2026-05-06 16:17:07 +0800

    Update version number to v1.3

--- _meta.lua
+++ _meta.lua
@@ -3,6 +3,6 @@ return {
     name = "pinyin_enhancement",
     fullname = _("拼音输入法增强"),
     description = _("在拼音输入法上方显示候选词栏，点击即可输入汉字"),
-    version = "v1.2",
+    version = "v1.3",
     author = "gytwo",
 }
